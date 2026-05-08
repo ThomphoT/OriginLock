00917d5574f078638d7cf2a1cb8d00a1740dfd16
ThomphoT <thomphotshivenga@gmail.com>
2026-05-08 13:40:26 +0000

    Backend File Structure

---
+++ origin-lock/src/main/java/com/nexus/origin/lock/OriginLockApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.origin.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriginLockApplication.class, args);
+	}
+
+}
---
+++ origin-lock/src/test/java/com/nexus/origin/lock/OriginLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.origin.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
